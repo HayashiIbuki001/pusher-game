7eac062909964ef858e2c872e055ae0b2b4e2bc5
HayashiIbuki001 <sch-w4meleer@outlook.jp>
2025-07-04 19:44:37 +0900

    プッシャー修正

--- Assets/Script/PusherController.cs
+++ Assets/Script/PusherController.cs
@@ -17,22 +17,22 @@ public class PusherController : MonoBehaviour
         transform.position = startPos + Vector3.forward * Mathf.Sin(Time.time * speed) * distance;
     }
 
-    private void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.tag == "Coin")
-        {
-            // プッシャーの子オブジェクトにする
-            other.gameObject.transform.parent = this.transform;
-            other.transform.SetParent(this.transform, false);
-        }
-    }
+    //private void OnCollisionEnter(Collision other)
+    //{
+    //    if (other.gameObject.tag == "Coin")
+    //    {
+    //        // プッシャーの子オブジェクトにする
+    //        other.gameObject.transform.parent = this.transform;
+    //        other.transform.SetParent(this.transform, false);
+    //    }
+    //}
 
-    private void OnCollisionExit(Collision other)
-    {
-        if (other.gameObject.tag == "Coin")
-        {
-            // 親離れ
-            other.gameObject.transform.parent = null;
-        }
-    }
+    //private void OnCollisionExit(Collision other)
+    //{
+    //    if (other.gameObject.tag == "Coin")
+    //    {
+    //        // 親離れ
+    //        other.gameObject.transform.parent = null;
+    //    }
+    //}
 }
